cf697dc3b906edef1564020a069dde537ff80ed3
Karthikeyan N <Karthikn@KarthiKn.onmicrosoft.com>
2026-05-22 21:01:51 +0530

    backend url fixed

--- src/app/services/reel.service.ts
+++ src/app/services/reel.service.ts
@@ -9,7 +9,7 @@ export class ReelService {
 
   // STABLE PRODUCTION BACKEND
   private apiBase =
-    'https://instagram-downloader--karthikn2593.replit.app/api/reels';
+'https://a0044a88-1064-4411-90b2-91cdae8595ba-00-2hvzb28y196eg.sisko.replit.dev/api/reels';
 
   constructor(private http: HttpClient) {}
 
